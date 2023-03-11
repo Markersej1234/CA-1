02cf9f9e5d269e41199ad11470b945e8c6670e25
Arsim Ljoki <113034331+ArsimBoy420@users.noreply.github.com>
2023-03-11 10:42:44 +0100

    wtf

--- src/main/java/entities/Phone.java
+++ src/main/java/entities/Phone.java
@@ -1,4 +1,58 @@
 package entities;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phone")
 public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "number", length = 30)
+    private String number;
+
+    @Column(name = "description", length = 30)
+    private String description;
+
+    // many to one person
+    @ManyToOne
+    @JoinColumn(name = "phone_id")
+    private Phone phone;
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
