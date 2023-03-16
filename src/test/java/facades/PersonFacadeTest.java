8ca9d81696a94906ed53981383f387929e015e53
Mark Gonzales Nielsen <cph-mn651@cphbusiness.dk>
2023-03-16 12:17:49 +0100

    Rest test IntTest

---
+++ src/test/java/facades/PersonFacadeTest.java
@@ -0,0 +1,67 @@
+package facades;
+
+import dtos.PersonDTO;
+import entities.Person;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PersonFacadeTest {
+
+
+    private static EntityManagerFactory emf;
+    private static PersonFacade facade;
+    private Person m1, m2;
+
+
+    @BeforeAll
+    public static void setUpClass()
+    {
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+        facade = PersonFacade.getInstance(emf);
+
+    }
+
+    @BeforeEach
+    void setUp()
+    {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Person.deleteAllRows").executeUpdate();
+            m1 = new Person("Hans","Bo", "mark@gmail.com");
+            m2 = new Person("Super" , "Hans", "hans@bo.dk");
+
+            em.persist(m1);
+            em.persist(m2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    void getAllProjects()  {
+        List<PersonDTO> boatDTOList = facade.getAllPersons();
+        int expected = 2;
+        int actual = boatDTOList.size();
+        assertEquals(expected, actual);
+        assertThat(boatDTOList, containsInAnyOrder(new PersonDTO(m1), new PersonDTO(m2)));
+    }
+
+//    @Test
+//    void createProject() {
+//        //ændrer i PersonDTO mangler id hvad fuck er long, det burde bare være et tal
+//        facade.createPerson(new PersonDTO("Hans", "Hans", "Hans@Hans", "1234"));
+//        assertEquals(3, facade.getPersonCount());
+//    }
+}
---
+++ src/test/java/rest/PersonResourseTest.java
@@ -0,0 +1,121 @@
+package rest;
+
+import dtos.PersonDTO;
+import entities.Person;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PersonResourseTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+    private static Person t1, t2;
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        //This method must be called before you request the EntityManagerFactory
+        EMF_Creator.startREST_TestWithDB();
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+
+        httpServer = startServer();
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //System.in.read();
+
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+        httpServer.shutdownNow();
+    }
+
+    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        t1 = new Person("Big Hans","Hans","Hans@Hans.dk");
+        t2 = new Person("Hans","Hansi", "Hansi@wurst.com");
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Person.deleteAllRows").executeUpdate();
+            em.persist(t1);
+            em.persist(t2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    public void testServerIsUp() {
+        given().when().get("/xxx").then().statusCode(200);
+    }
+
+    //husk hashcode og equals i DTO
+    @Test
+    void getAll()
+    {
+        List<PersonDTO> projectDTOS;
+
+        projectDTOS = given()
+                .contentType("application/json")
+                .when()
+                .get("/person/all")
+                .then()
+                .extract().body().jsonPath().getList("", PersonDTO.class);
+
+        PersonDTO p1 = new PersonDTO(t1);
+        PersonDTO p2 = new PersonDTO(t2);
+        assertThat(projectDTOS, containsInAnyOrder(p1,p2));
+    }
+
+    @Test
+    void createProject() {
+        //DTOen skal nok også ændres her
+        given()
+                .contentType(ContentType.JSON)
+                .body(new PersonDTO(t1))
+                .when()
+                .post("person/createperson")
+                .then()
+                .body("email", equalTo("Hans@Hans.dk "))
+                .body("firstname", equalTo("Hans "))
+                .body("lastname", equalTo("Hans"))
+                .body("id", notNullValue());
+    }
+}
